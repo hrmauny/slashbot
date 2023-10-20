53773cf1afb7f77dc80a923d5a826af5317cea26
Jay Thakur <jsthakur@ncsu.edu>
2023-10-19 20:24:29 -0400

    Update test_summary.py

--- test/bot/test_summary.py
+++ test/bot/test_summary.py
@@ -28,26 +28,6 @@ class TestSummary(BotTest):
         # there should not be any exceptions
         assert self.bot.worker_pool.exception_info is None
 
-    def test_history_command_records(self):
-        """
-        Tests the history command
-        """
-        self.create_record(10.00)
-        msg = self.create_text_message('/history')
-        self.bot.process_new_messages([msg])
-        time.sleep(3)
-
-        # assert the message was sent, and text was not changed
-        assert msg.chat.id is not None
-        assert msg.text == '/history'
-        # there should be a next step handler
-        assert len(self.bot.next_step_backend.handlers) == 0, \
-            "For the /history command, there should not be a next step"
-        # there should not be any exceptions
-        assert self.bot.worker_pool.exception_info is None
-
-
-
 
 if __name__ == '__main__':
     unittest.main()
